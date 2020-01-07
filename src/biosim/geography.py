37135f6c54016aea77d7685e499c2159b5569055
Hemanth Sana <hemanth.babu.sana@nmbu.no>
2020-01-07 12:49:27 +0100

    geography

---
+++ src/biosim/geography.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+"""
+"""
+
+__author__ = "Hemanth Sana & Mithunan Sivagnanam"
+__email__ = "hesa@nmbu.no & misi@nmbu.no"
+
+
+class Ocean:
+    """
+        This represents the cells covered by ocean
+    """
+    def __init__(self):
+        pass
+
+
+class Jungle:
+    """
+        This represents the cells covered by jungle
+    """
+    fodder_max = 800
+
+    def __init__(self):
+        pass
+
+
+class Savannah:
+    """
+    This represents the cells covered by savannah
+    """
+    fodder_max = 300
+
+    def __init__(
+            self
+    ):
+        pass
+
+
+class Desert:
+    """
+        This represents the cells covered by desert
+    """
+    def __init__(self):
+        pass
+
+
+class Mountain:
+    """
+        This represents the cells covered by mountain
+    """
+    def __init__(self):
+        pass
--- src/biosim/simulation.py
+++ src/biosim/simulation.py
@@ -3,8 +3,8 @@
 """
 """
 
-__author__ = ""
-__email__ = ""
+__author__ = "Hemanth Sana & Mithunan Sivagnanam"
+__email__ = "hesa@nmbu.no & misi@nmbu.no"
 
 
 class BioSim:
